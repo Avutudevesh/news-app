2cd4a8bb1266c4458ab088a34fcac452ababdd61
Devesh Avutu <devesh14may@gmail.com>
2020-03-18 21:22:19 +0530

    using glide to show images

--- app/src/main/java/com/example/news_app/view/NewsViewHolder.java
+++ app/src/main/java/com/example/news_app/view/NewsViewHolder.java
@@ -4,6 +4,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.news_app.R;
 import com.example.news_app.models.NewsData;
 
@@ -19,11 +21,24 @@ class NewsViewHolder extends RecyclerView.ViewHolder {
     private TextView source = itemView.findViewById(R.id.source);
     private TextView title = itemView.findViewById(R.id.title);
     private TextView publishDate = itemView.findViewById(R.id.publish_date);
-    private ImageView headlineImage = itemView.findViewById(R.id.headline_image);
+    private ImageView newsImage = itemView.findViewById(R.id.headline_image);
 
     void bind(NewsData data) {
         source.setText(data.getSource().getName());
         title.setText(data.getTitle());
         publishDate.setText(data.getPublishedAt().toString());
+        loadBackgroundImage(data.getUrlToImage());
+    }
+
+    private void loadBackgroundImage(String url) {
+        if (url != null) {
+            Glide.with(itemView)
+                    .load(url)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .dontAnimate()
+                    .fitCenter()
+                    .into(newsImage);
+        }
+
     }
 }
